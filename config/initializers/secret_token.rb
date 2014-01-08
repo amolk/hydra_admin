eb8f034a9bbdb50f5cb0c95a22b2f0b905657bd8
Amol Kelkar <kelkar.amol@gmail.com>
2014-01-07 16:21:53 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HydraAdmin::Application.config.secret_key_base = 'a021aed6a19858434be597cccd3d9aa92566469ffac5afd0290de56d1bf3dbf950344397b785d072c39fc97afa028941b6555f1dca091af656850e78374ceea1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HydraAdmin::Application.config.session_store :cookie_store, key: '_hydra_admin_session'
